a549edcbf42858d65c414ac21e476c7acf56a7f4
Sakshaug, Johan <Johan.Sakshaug@skatteetaten.no>
2023-08-16 09:25:16 +0200

    SEK-6613 - oversettelse driftsvarsel-side

---
+++ nyheter-og-driftsvarsler/_varsler-ingress-en.md
@@ -0,0 +1,3 @@
+We publish changes to documentation, technical solutions and news/alerts on this page.
+
+News/alerts that should be communicated directly, will be sent to the notification address provided during the conclusion of the agreement. It is the consumers responsibility to ensure the Tax Administration has the correct address for these alerts.
--- src/theme/BlogPostItems/index.tsx
+++ src/theme/BlogPostItems/index.tsx
@@ -7,8 +7,20 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 import Ingress from "../../../nyheter-og-driftsvarsler/_varsler-ingress.md";
 import HvordanAbonnere from "../../../nyheter-og-driftsvarsler/_varsler-hvordan-abonnere.md";
+import IngressEN from "../../../nyheter-og-driftsvarsler/_varsler-ingress-en.md";
+import HvordanAbonnereEN from "../../../nyheter-og-driftsvarsler/_varsler-hvordan-abonnere-en.md";
 
 import styles from "./BlogPostItems.module.scss";
+import Translate, { translate } from "@docusaurus/Translate";
+
+const hentTekster = (locale: string) => {
+  switch (locale) {
+    case "nb":
+      return { ingress: <Ingress />, hvordanAbonnere: <HvordanAbonnere /> };
+    case "en":
+      return { ingress: <IngressEN />, hvordanAbonnere: <HvordanAbonnereEN /> };
+  }
+};
 
 const columns = [
   {
@@ -34,6 +46,8 @@ export default function BlogPostItems({
 }) {
   const context = useDocusaurusContext();
 
+  const { ingress, hvordanAbonnere } = hentTekster(context.i18n.currentLocale);
+
   const data = items.map((varsel) => ({
     date: varsel.content.metadata.formattedDate,
     title: (
@@ -47,19 +61,29 @@ export default function BlogPostItems({
   const configTitleString = context.siteConfig.title
     ? ` - ${context.siteConfig.title}`
     : "";
-  const title = `Nyheter og driftsvarsler${configTitleString}`;
+  const title = `${translate({
+    message: "Nyheter og driftsvarsler",
+  })}${configTitleString}`;
 
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <h1>Nyheter og driftsvarsler</h1>
-      <h2>Abonnere på nyheter og driftsvarsler</h2>
-      <Ingress />
-      <h2>Hvordan abonnere på varsler</h2>
-      <HvordanAbonnere />
-      <h2>Nyheter og driftsvarsler</h2>
+      <h1>
+        <Translate>Nyheter og driftsvarsler</Translate>
+      </h1>
+      <h2>
+        <Translate>Abonnere på nyheter og driftsvarsler</Translate>
+      </h2>
+      {ingress}
+      <h2>
+        <Translate>Hvordan abonnere på varsler</Translate>
+      </h2>
+      {hvordanAbonnere}
+      <h2>
+        <Translate>Nyheter og driftsvarsler</Translate>
+      </h2>
 
       <BrowserOnly>
         {() => {
@@ -70,7 +94,7 @@ export default function BlogPostItems({
             <Table
               data={data}
               columns={columns}
-              caption="Nyheter og driftsvarsler"
+              caption={translate({ message: "Nyheter og driftsvarsler" })}
               hideCaption
             />
           );
